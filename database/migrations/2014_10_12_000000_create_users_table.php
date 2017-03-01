04679ad32af99b59cfc26eb35c97943e1493be18
Siwawes Wongcharoen <siwaoh@gmail.com>
2017-03-01 22:47:29 +0700

    update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,16 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string('surname')->nullable();
+            $table->string('nickname')->nullable();
+            $table->string('gender')->nullable();
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('education')->nullable();
+            $table->string('biography')->nullable();
+            $table->date('date_of_birth')->nullable();
+            $table->string('website')->nullable();
+            $table->string('country')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
